2f24655f1d45adc4590652b0ffa00fb3f490c3f4
Oleksandr-Marych <162808223+Oleksandr-Marych@users.noreply.github.com>
2024-08-29 14:08:41 +0200

    initial commit

---
+++ Voyage_Haven_website/src/main/java/com/example/voyage_haven_website/VoyageHavenWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.voyage_haven_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageHavenWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyageHavenWebsiteApplication.class, args);
+	}
+
+}
---
+++ Voyage_Haven_website/src/test/java/com/example/voyage_haven_website/VoyageHavenWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voyage_haven_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageHavenWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
